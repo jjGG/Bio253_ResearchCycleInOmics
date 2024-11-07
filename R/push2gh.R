cb203729a46704970bd12a27621231eee1930852
Jonas Grossmann <jonas.grossmann@fgcz.uzh.ch>
2024-11-07 11:00:17 +0100

    script to push a local repos to gh

---
+++ R/push2gh.R
@@ -0,0 +1,10 @@
+#!/usr/local/bin/Rscript
+#
+# Jonas Grossmann <jg@fgcz.ethz.ch>
+# 2024
+#
+#
+
+# push a repos that is prepared locally in Rstudio to gh incase your Rstudio is already connected to gh
+library(usethis)
+use_github()
