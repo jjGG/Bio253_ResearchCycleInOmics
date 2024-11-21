ef1911947601543ef66ea44e5d2d1bab3251d53e
Natalia Zajac <natalia.zajac@fgcz.ethz.ch>
2024-11-21 09:10:17 +0100

    pca script improved

--- R/genotype_PCA.r
+++ R/genotype_PCA.r
@@ -3,9 +3,10 @@ library(adegenet)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
+library(reshape2)
 
 #Load in a vcf
-vcf <- read.vcfR("../course_RCIG/JE2_set_forIGV/2020-09-25_mod_SAJE2_filteredFromAncestor_noOutlier.1.vcf", verbose = FALSE)
+vcf <- read.vcfR("../../../course_Research cycle in genomics/JE2_set_forIGV/2020-09-25_mod_SAJE2_filteredFromAncestor_noOutlier.1.vcf", verbose = FALSE)
 
 #Convert a vcf to genind object and then to df
 x <- vcfR2genind(vcf, return.alleles = TRUE, ploidy = 1, sep = "/")
@@ -24,6 +25,7 @@ metadata = metadata[!metadata$Clone == "P",]
 #Run PCA
 pca1 <- dudi.pca(df = xT, center = TRUE, scale = FALSE, scannf = FALSE, nf = 15)
 
+#Create PCA with labels
 s.label(pca1$li)
 
 #Plot a PCA
@@ -33,6 +35,30 @@ plot(pca1$eig)
 loadingplot(pca1$c1, axis=1, thres=.07, lab.jitter=1)
 loadingplot(pca1$c1, axis=2, thres=.07, lab.jitter=1)
 
+#Phenotype
+phenotype = readxl::read_xlsx("../../../course_Research cycle in genomics/resources/SA_evolution_phenotypes_v2_noCowan.xlsx")
+phenotype = phenotype[phenotype$SA_strain_ID == "JE2",]
+phenotype = phenotype %>% 
+  mutate(sample = if_else(evolution_condition == "supernatant", paste0(SA_strain_ID,"_", "S"), 
+                          paste0(SA_strain_ID,"_", "T"))) 
+phenotype$sample_ID = if_else(nchar(sapply(str_split(phenotype$strain_ID, "\\."), .subset,1)) == 2, 
+                              paste0(sapply(str_split(sapply(str_split(phenotype$strain_ID, "\\."), .subset,1), ""), .subset, 1), "0", sapply(str_split(sapply(str_split(phenotype$strain_ID, "\\."), .subset,1), ""), .subset, 2)), sapply(str_split(phenotype$strain_ID, "\\."), .subset,1))
+phenotype$sample_ID_2 = sapply(str_split(phenotype$strain_ID, "\\."), .subset,2)
+phenotype$sample =  paste0(phenotype$sample, "_", phenotype$sample_ID, "_", phenotype$sample_ID_2)
+phenotype = phenotype %>% column_to_rownames("sample")
+temp = merge(pca1$li, phenotype, by = "row.names")
+melt(temp, id.vars = colnames(temp)[c(1,17:33)]) %>% ggplot(aes(value, average_H2O2_survival, color = variable)) + geom_line()
+
+##Calculate the correlation of a PC with a particular phenotype
+correlations <- sapply(temp[, 2:16], function(pc) cor(as.numeric(pc), temp$average_H2O2_survival, use = "complete.obs"))
+
+# Sort and view top correlations
+sort(abs(correlations), decreasing = TRUE)[1:10]
+
 #Create a plot using ggplot, labelling points according to metadata
 merge(pca1$li, metadata, by = "row.names") %>% 
   ggplot(aes(Axis3, Axis4, color = Treatment)) + geom_point()
+
+
+merge(pca1$li, phenotype, by = "row.names") %>% data.frame() %>%
+  ggplot(aes(Axis3, Axis4, color = average_H2O2_survival)) + geom_point()
